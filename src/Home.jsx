ad83afe5be5637127613c279635185e6716a9a7b
AmalJAntony <amaljantonyp@gmail.com>
2026-06-08 09:45:37 +0530

    feat/card styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,20 +12,19 @@ function Home() {
             )
         )
 
-
-
     }
     return (
         apiResponse ?
-
             <main className='w-full bg-zinc-600 flex flex-col justify-center items-center min-h-screen h-[100%]'>
-                <section className='grid grid-cols-1 md:grid-cols-3 justify-center container xl:max-w-[1400px] gap-10'>
+                <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-10 justify-center container xl:max-w-[1400px] gap-10'>
                     {apiResponse?.map((item, index) => (
     
                         <div className="flex flex-col justify-start items-start border-1 border-white text-white shadow p-5 gap-3 rounded-3xl">
-                            <p>#{item.id}</p>
+                            <div className='flex gap-5 p-3 border-1 bg-zinc-500 w-full rounded-3xl'>
+                                <p className='border rounded-full p-2 bg-white text-black'>#{item.id}</p>
+                                <p>{item.email}</p>
+                            </div>
                             <p className='text-3xl font-bold'>{item.name}</p>
-                            <p>{item.email}</p>
                             <p>{item.body}</p>
     
                         </div>
@@ -39,13 +38,7 @@ function Home() {
                 <div className='container xl:max-w-[1400px] flex justify-center items-center shadow'>
                     <button onClick={apiCall} className='border-1 border-white rounded p-4 text-white text-xl cursor-pointer'>Load Comments</button>
                 </div>
-
-
-
             </section>
-
-
-
     )
 }
 
